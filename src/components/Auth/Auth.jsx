7cb47ff7adcbb2754aac98e183385d0752973f74
Christophe REALINI <rp.chezchris@gmail.com>
2022-03-25 12:38:01 +0100

    Revert "[TASK-004] Link Login to firebase"

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,14 +1,12 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import Login from './Login/Login'
 import Register from './Register/Register'
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 
-
-
 const Auth = () => {
   return (
     <>   
-    <h1 style={{color: "white"}}>User</h1>
+    <h1>User</h1>
       <nav>
         <Link to="">Profile</Link>
       </nav>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { UserContextProvider } from './context/userContext';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
