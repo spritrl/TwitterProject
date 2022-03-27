ce604a541cf134df23d7a4a98b07e4b6991a3fb7
Mouad Anouch <mouad.anouch7@gmail.com>
2022-03-27 22:41:35 +0200

    Mouad: fix routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
@@ -6,19 +6,14 @@ import Login from './components/Auth/Login/Login';
 import Hold from './components/hold/Hold';
 import Auth from './components/Auth/Auth';
 import Register from './components/Auth/Register/Register';
-import { UserContextProvider } from './context/userContext';
+import { UserContext, UserContextProvider } from './context/userContext';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import TimelineView from './views/timelineView';
 import ProfileView from './views/profileView';
 import Profile from './components/profile';
-// import { Routes, Route, Link } from "react-router-dom";
-// import Login from './components/Auth/Login/Login';
-// import Hold from './components/hold/Hold';
-// import Auth from './components/Auth/Auth';
-// import Register from './components/Auth/Register/Register';
-// import { UserContextProvider } from './context/userContext';
+
 
 function App() {
   useEffect(() => {
@@ -30,17 +25,17 @@ function App() {
         console.warn(documents);
       })
   }, []);
+  const { currentUser } = useContext(UserContext);
 
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Hold />} /> */}
         <Route path="/" element={<TimelineView />} />
-        {/* <Route path="/profile" element={<ProfileView />} /> */}
+        <Route path="/profile" element={<ProfileView />} />
         <Route path="auth" element={<Auth />}>
           <Route index element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="profile" element={<ProfileView />} />
+          <Route path={`profile`} element={<ProfileView />} />
         </Route>
       </Routes>
     </>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -112,7 +112,7 @@ const Navbar = ({ route, navigation, username }) => {
         </SvgIcon>
         <a style={text}>Home</a>
       </Button>
-      <Button style={button} component={Link} to={`/auth/profile`}>
+      <Button style={button} component={Link} to={`/auth/profile?username=${currentUser.email}`}>
         <SvgIcon style={svg} viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
           <g stroke="none" strokeWidth="1" fill={'white'} fillRule="evenodd"><path d="M12.225 12.165c-1.356 0-2.872-.15-3.84-1.256-.814-.93-1.077-2.368-.805-4.392.38-2.826 2.116-4.513 4.646-4.513s4.267 1.687 4.646 4.513c.272 2.024.008 3.46-.806 4.392-.97 1.106-2.485 1.255-3.84 1.255zm5.849 9.85H6.376c-.663 0-1.25-.28-1.65-.786-.422-.534-.576-1.27-.41-1.968.834-3.53 4.086-5.997 7.908-5.997s7.074 2.466 7.91 5.997c.164.698.01 1.434-.412 1.967-.4.505-.985.785-1.648.785z"></path></g>
         </SvgIcon>
